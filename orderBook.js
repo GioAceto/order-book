7007337fb66434c36047427064a69d8289f91c2c
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-11 19:58:17 -0400

    add to function so it utilizes validateType function to check whether a deal can be made and if not pushes to array

--- orderBook.js
+++ orderBook.js
@@ -1,33 +1,50 @@
 const reconcileOrder = (existingBook, incomingOrder) => {
-  let result = []
+  let book = []
 
-  // checks if the order book is empty or only has orders of one type in it, adds incoming order to the back
-  // of the order book
-  if (existingBook.length > 0) {
-    result = 'test'
+  if (existingBook.length === 0) {
+    book.push(incomingOrder)
   } else {
-    result.push(incomingOrder)
+    if (validateType(existingBook, incomingOrder) === false) {
+      book = existingBook.concat(incomingOrder)
+    }
   }
 
-  return result
-
-
-  // if the order book does not have any matching price to the incoming buy or sell order, add the incoming 
-  // order to the back of the order book
-
-  // if incoming order matches price and qty to an existing order of a different type in the order book,
-  // then the sale is executed and the existing order is removed from the order book
-
-  // if the incoming order matches an existing order price of an order with a different type in the order 
-  // book, and the qty is less, subtract the incoming order qty from the existing order qty and keep the 
-  // remainder in the order book.
-
-  // move the partially executed existing order to the back of the order book
-
-  // if the incoming order matches an existing order price in the order book, and the qty is greater,
-  // the sale is partially executed so that the existing order is subtracted from the incoming order
-
-  // Move the remainder of the partially executed incoming order to the end of the order book
+  return book
 }
 
+const validateType = (existingBook, incomingOrder) => {
+  let deal = true
+
+  for (let i = 0; i < existingBook.length; i++) {
+    if (existingBook[i].type === incomingOrder.type) {
+      deal = false
+    }
+  }
+
+  return deal
+}
+
+
 module.exports = reconcileOrder
+
+
+// checks if the order book is empty or only has orders of one type in it, adds incoming order to the back
+// of the order book
+
+// if the order book does not have any matching price to the incoming buy or sell order, add the incoming 
+// order to the back of the order book
+
+// if incoming order matches price and qty to an existing order of a different type in the order book,
+// then the sale is executed and the existing order is removed from the order book
+
+// if the incoming order matches an existing order price of an order with a different type in the order 
+// book, and the qty is less, subtract the incoming order qty from the existing order qty and keep the 
+// remainder in the order book.
+
+// move the partially executed existing order to the back of the order book
+
+// if the incoming order matches an existing order price in the order book, and the qty is greater,
+// the sale is partially executed so that the existing order is subtracted from the incoming order
+
+// Move the remainder of the partially executed incoming order to the end of the order book
+
