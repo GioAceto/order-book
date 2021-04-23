ce6ee64e11084516765836be56108ca4654c5cef
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-23 17:25:37 -0400

    change back parameter names

--- orderBook.js
+++ orderBook.js
@@ -1,74 +1,81 @@
 // const hasDiffType = require('./has-different-type')
 
-const reconcileOrder = (book, order) => {
+const reconcileOrder = (existingBook, incomingOrder) => {
   let empty = []
 
-  if (book.length === 0) {
-    empty.push(order)
-  } else if (book.length > 0) {
-    empty = book
-    if (checksAllType(book, order) === false) {
-      empty.push(order)
-    } else if (checksAllPrice(book, order) === false) {
-      empty.push(order)
-    } else {
-      for (let i = 0; i < book.length; i++) {
-        // fulfills perfect deal
-        if (book[i].type !== order.type && book[i].price === order.price && book[i].quantity === order.quantity) {
-          empty.shift(book[i])
-        }
-        // fulfills deal of corresponding type and price, with a lower order quantity than the existing book order
-        if ((book[i].type !== order.type) && (book[i].price === order.price) && (book[i].quantity > order.quantity)) {
-          book[i].quantity -= order.quantity
-          empty.push(empty.shift(book[i]))
-        }
-        // fulfills deal of corresponding type and price, with a greater order quantity than the existing book order
-        if ((book[i].type !== order.type) && (book[i].price === order.price) && (book[i].quantity < order.quantity)) {
-          order.quantity = (book[i].quantity -= order.quantity) * -1
-          book.push(order)
-          book.shift(book[i])
-        }
-      }
-    }
+  if (existingBook.length === 0) {
+    empty.push(incomingOrder)
   }
+
+  if (existingBook.length > 0) {
+    empty.push(incomingOrder)
+  }
+
+  //   if (checksAllType(book, order) === false) {
+  //     empty.push(order)
+  //   } else if (checksAllPrice(book, order) === false) {
+  //     empty.push(order)
+  //   } else {
+  //     for (let i = 0; i < book.length; i++) {
+  //       // fulfills perfect deal
+  //       if (book[i].type !== order.type && book[i].price === order.price && book[i].quantity === order.quantity) {
+  //         empty.shift(book[i])
+  //       }
+  //       // fulfills deal of corresponding type and price, with a lower order quantity than the existing book order
+  //       if ((book[i].type !== order.type) && (book[i].price === order.price) && (book[i].quantity > order.quantity)) {
+  //         book[i].quantity -= order.quantity
+  //         empty = book.splice(book[i], 1)
+  //         empty.unshift(...book)
+  //       }
+  //       // fulfills deal of corresponding type and price, with a greater order quantity than the existing book order
+  //       if ((book[i].type !== order.type) && (book[i].price === order.price) && (book[i].quantity < order.quantity)) {
+  //         empty = book.shift()
+  //         book[i].quantity -= order.quantity
+  //         book.splice(1, 0, empty)
+  //       }
+  //     }
+  //   }
+  // }
 
   return empty
 }
 
-const checksAllType = (book, order) => {
-  let deal = false
-  let count = 0
-
-  for (let i = 0; i < book.length; i++) {
-    if (book[i].type !== order.type) {
-      count++
-    }
-    if (count > 0) {
-      deal = true
-    }
-  }
-
-  return deal
-}
-
-const checksAllPrice = (book, order) => {
-  let deal = false
-  let count = 0
-
-  for (let i = 0; i < book.length; i++) {
-    if ((book[i].type !== order.type) && (book[i].price === order.price)) {
-      count++
-    }
-    if (count > 0) {
-      deal = true
-    }
-  }
-
-  return deal
-}
 
 
-module.exports = reconcileOrder
+// const checksAllType = (book, order) => {
+//   let deal = false
+//   let count = 0
+
+//   for (let i = 0; i < book.length; i++) {
+//     if (book[i].type !== order.type) {
+//       count++
+//     }
+//     if (count > 0) {
+//       deal = true
+//     }
+//   }
+
+//   return deal
+// }
+
+// const checksAllPrice = (book, order) => {
+//   let deal = false
+//   let count = 0
+
+//   for (let i = 0; i < book.length; i++) {
+//     if ((book[i].type !== order.type) && (book[i].price === order.price)) {
+//       count++
+//     }
+//     if (count > 0) {
+//       deal = true
+//     }
+//   }
+
+//   return deal
+// }
+
+
+// module.exports = reconcileOrder
 
 
 // checks if the order book is empty or only has orders of one type in it, adds incoming order to the back
